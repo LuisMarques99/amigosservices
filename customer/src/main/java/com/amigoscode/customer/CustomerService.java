196aa0976c87ea89814ce1adc6b9196370e6ab7f
Luis Marques <luisserafim99@hotmail.com>
2023-05-27 17:54:37 +0100

    Add fraud check when new Customer is registered

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -1,9 +1,10 @@
 package com.amigoscode.customer;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public record CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
     public void registerCustomer(final CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -12,8 +13,16 @@ public record CustomerService(CustomerRepository customerRepository) {
                 .build();
         // TODO: Check if email is valid
         // TODO: Check if email is not taken
-        // TODO: Check if customer is a fraudster
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+        assert fraudCheckResponse != null : "Fraud service couldn't be reached.";
+        if (fraudCheckResponse.isFraudster()){
+            throw new IllegalStateException("Customer is fraudster.");
+        }
         // TODO: Send notification
     }
 }
